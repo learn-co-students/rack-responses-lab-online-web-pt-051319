9ec43d3abe11a97ef33c5e1e699e16dcfce531ca
Kelley Chaplain <kmchaplain@gmail.com>
2019-07-30 21:02:33 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+
+class Application
+  
+  
+  def call(env)
+    
+    resp = Rack::Response.new
+
+    case
+    when Time.now.hour.between?(1,12)
+      resp.write "Good Morning!"
+    when Time.now.hour.between?(13,24)  
+      resp.write "Good Afternoon!"
+    end  
+
+    resp.finish
+  end  
+
+end  
