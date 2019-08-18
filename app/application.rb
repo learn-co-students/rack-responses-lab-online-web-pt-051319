a6b3d01009ced79e8ca1041cc053acff5191c8ef
James Britton <brittonjp@gmail.com>
2019-08-18 14:01:56 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,16 @@
 class Application
-
-    def call(env)
-        resp = Rack::Response.new
-        
-        case
-        when Time.now.hour.between?(1,12)
-          resp.write "Good Morning!"
-        when Time.now.hour.between?(13,24)  
-          resp.write "Good Afternoon!"
-        end  
   
-        resp.finish
+  def call(env)
+    resp = Rack::Response.new
+    
+    time = Time.now
+    
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
     end
-  
+    resp.finish
   end
   
+end
